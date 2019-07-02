eec2aeeacc1d9ebd91c0627ce0353a903c405b42
Samarth Dave <samarthdave@users.noreply.github.com>
2019-07-02 11:54:11 -0500

    Add font and text styles

--- App.js
+++ App.js
@@ -4,12 +4,22 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.headerStyle}>donate</Text>
+      
+      <Text style={styles.subtitleStyle}>A tangible solution to reduce food waste.</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  headerStyle: {
+    color: '#444',
+    fontSize: 40,
+  },
+  subtitleStyle: {
+    color: '#333',
+    fontSize: 15,
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
